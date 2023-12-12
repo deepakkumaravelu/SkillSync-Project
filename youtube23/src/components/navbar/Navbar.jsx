be1c11e324003b2b75b826d2b2fca8d032528ea1
deepakkumaravelu <deepak1122003kumar@gmail.com>
2023-12-12 11:10:19 +0530

    navbar drop down added

--- youtube23/src/components/navbar/Navbar.jsx
+++ youtube23/src/components/navbar/Navbar.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.scss";
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "Deepak",
+    isSeller: true,
+  };
   return (
-    <div className="navbar">
+    <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
           <span className="text">SkillSync</span>
@@ -12,10 +30,41 @@ const Navbar = () => {
           <span>Explore</span>
           <span>English</span>
           <span>Sign in</span>
-          <span>Become a Seller</span>
-          <button>Join</button>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={() => setOpen(!open)}>
+              <img
+                src="https://avatars.githubusercontent.com/u/67898647?v=4"
+                alt=""
+              />
+              <span>{currentUser?.username}</span>
+              {open && (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add New Gig</span>
+                    </>
+                  )}
+                  <span>Orders</span>
+                  <span>Messages</span>
+                  <span>Logout</span>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test</span>
+            <span>Test1</span>
+          </div>
+        </>
+      )}
     </div>
   );
 };
