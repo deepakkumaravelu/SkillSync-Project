f02be0def08ce48e1be51e329989914fa0517a56
deepakkumaravelu <deepak1122003kumar@gmail.com>
2023-12-13 15:43:58 +0530

    video embedded

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,15 +1,14 @@
-import React from "react";
+import React, { Children } from "react";
 import "./Slide.scss";
 import Slider from "infinite-react-carousel";
-import CatCard from "../catCard/CatCard";
-import {cards} from "../../data"
-const Slide = () => {
+
+const Slide = ({children,slidesToShow,arrowsScroll}) => {
   return (
     <div className="slide">
       <div className="container">
-      <Slider slidesToShow={5} arrowsScroll={5}>
-      {cards.map(card=>(<CatCard item={card} key={card.id}/>))}
-    </Slider>
+      <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
+      {children}
+       </Slider>
       </div>
     </div>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,66 @@
-import React from 'react'
-import "./Home.scss"
-import Featured from '../../components/featured/Featured'
-import TrustedBy from '../../components/trustedBy/TrustedBy'
-import Slide from '../../components/Slide/Slide'
+import React from "react";
+import "./Home.scss";
+import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/Slide/Slide";
+import { cards } from "../../data";
+import CatCard from "../../components/catCard/CatCard";
+
 const Home = () => {
   return (
-    <div className='home'>
-    <Featured/>
-    <TrustedBy/>
-    <Slide/>
+    <div className="home">
+      <Featured />
+      <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard item={card} key={card.id} />
+        ))}
+      </Slide>
+      <div className="features">
+        <div className="container">
+          <div className="item">
+            <h1>A whole of freelance talent at your fingertips</h1>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+          </div>
+          
+          <div className="item">
+            <video src="./img/video.mp4" controls></video>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
