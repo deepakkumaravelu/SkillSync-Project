9af46bc5541be21ef4f8b1ac8312d6f3f428a379
deepak <deepak1122003kumar@gmail.com>
2023-12-16 14:46:50 +0530

    Slide.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Home.scss";
 import Featured from "../../components/featured/Featured";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
-import Slide from "../../components/slide/Slide";
+import Slide from "../../components/slide/Slide.jsx";
 import CatCard from "../../components/catCard/CatCard";
 import ProjectCard from "../../components/projectCard/ProjectCard";
 import { cards, projects } from "../../data"; 
